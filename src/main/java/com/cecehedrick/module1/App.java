9f05814ede6431b1aa7828e450e5451838e5bd85
Cece Hedrick <cecehedrick@gmail.com>
2018-04-09 15:33:33 -0600

    adds AGitSubmodule git submodule

--- src/main/java/com/cecehedrick/module1/App.java
+++ src/main/java/com/cecehedrick/module1/App.java
@@ -1,6 +1,8 @@
 package com.cecehedrick.module1;
 
-//import com.cecehedrick.module1.coolPkg.Yolo;
+import com.cecehedrick.module1.AGitSubmodule.submodule.superCoolPkg.Yolo;
+
+
 /**
  * Hello world!
  *
@@ -10,9 +12,9 @@ public class App
     public static void main( String[] args )
     {
 
-//        System.out.println( "Hello World!" );
-//        String msg = "Yolo I guess";
-//        Yolo.cool(msg);
+        System.out.println( "Hello World!" );
+        String msg = "Yolo I guess";
+        Yolo.cool(msg);
 
     }
 }
